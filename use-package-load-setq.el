578a4a158af8db9d71dd365bf3884b2f079fd4ef
Emacs Watcher <emacswatcher@gmail.com>
2018-06-22 14:16:00 -0700

    Bump version

--- use-package-load-setq.el
+++ use-package-load-setq.el
@@ -4,7 +4,7 @@
 
 ;; Author: Emacs Watcher
 ;; Created: ew (2018-04-18):
-;; Version: 0.1
+;; Version: 0.2
 
 ;; This program is free software; you can redistribute it and/or
 ;; modify it under the terms of the GNU General Public License as
